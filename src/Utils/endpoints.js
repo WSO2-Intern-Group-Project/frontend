3d2e21292c9dea33bfaaf7a7e6a49ed29e602dc4
Akila <akila.99g@gmail.com>
2023-05-16 21:58:47 +0530

    Change userById endpoint URL

--- src/Utils/endpoints.js
+++ src/Utils/endpoints.js
@@ -10,4 +10,4 @@ const requestByIdAPIUrl = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-
 const setStatusOfRequest = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0/setStatusOfRequest"
 
 const addUserAPIUrl = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0/addUser"
-const getUserById = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0/getUserById"
+const userByIdAPIUrl = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0/userById"
