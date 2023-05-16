0004b90e55ae2b472d0e846af12985e584cfa4fe
Akila <akila.99g@gmail.com>
2023-05-16 21:44:48 +0530

    Add API endpoint URLs

--- src/Utils/endpoints.js
+++ src/Utils/endpoints.js
@@ -1,1 +1,13 @@
 // Endpoints
+const identityCheckAPIUrl = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/externalapis/endpoint-9090-803/1.0.0/identityRecordByNIC";
+const addressCheckAPIUrl = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/externalapis/endpoint-9090-803/1.0.0/residentsByAddress"
+const policeCheckAPIUrl = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/externalapis/endpoint-9090-803/1.0.0/policeRecordsByNIC"
+
+const addRequestAPIUrl = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0/addRequest"
+const requestsByUserAPIUrl = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0/requestsByUser"
+const requestsByDomainAPIUrl = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0/requestsByDomain"
+const requestByIdAPIUrl = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0/requestById"
+const setStatusOfRequest = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0/setStatusOfRequest"
+
+const addUserAPIUrl = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0/addUser"
+const getUserById = "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0/getUserById"
