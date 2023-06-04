7233f722736139621f62db543d5118b9aec7e3b8
Akila <akila.99g@gmail.com>
2023-06-04 20:53:37 +0530

    Fix resourceServerURLs with prod baseURL

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const config = {
   storage: "sessionStorage",
   resourceServerURLs: [
     "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/gramasevabackend/endpoint-9090-803/1.0.0",
-    "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-dev.e1-us-east-azure.choreoapis.dev/bhzm/externalapis/endpoint-9090-803/1.0.0",
+    "https://a7bf0dba-d37a-4f74-ab2a-11d52f500ed9-prod.e1-us-east-azure.choreoapis.dev/bhzm/externalapis/endpoint-9090-803/1.0.0/",
   ],
 };
 
